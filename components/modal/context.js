79087c37fd9abf320bf5819935ee8aa90edaaad7
FitFingers <hooper.j.1991@gmail.com>
2021-09-19 13:31:43 +0200

    feat: reset tx data on new modal

--- components/modal/context.js
+++ components/modal/context.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useReducer } from "react";
+import useTransaction from "hooks/useTransaction";
+import { createContext, useCallback, useContext, useReducer } from "react";
 import Modal from "./index";
 
 // ===================================================
@@ -16,6 +17,7 @@ export default function useModal() {
 // ===================================================
 
 export function ModalContext({ children }) {
+  const { updateTransaction } = useTransaction();
   const [
     { open, title, description, contractFunction, formDefaults, callback },
     dispatch,
@@ -28,22 +30,26 @@ export function ModalContext({ children }) {
     callback: () => {},
   });
 
-  const handleOpen = ({
-    title: newTitle,                // Modal heading
-    description: newDescription,    // Modal subheading
-    contractFunction: newType,      // Name of function as it appears in the smart contract
-    formDefaults: newFormDefaults,  // JSON Object of default field values (used to pass dynamic initial vars)
-    callback: newCallback,          // Function to call on submit (allows custom construction of params etc)
-  }) => {
-    dispatch({
-      open: true,
-      title: newTitle,
-      description: newDescription,
-      contractFunction: newType,
-      formDefaults: newFormDefaults,
-      callback: newCallback,
-    });
-  };
+  const handleOpen = useCallback(
+    ({
+      title: newTitle,                // Modal heading
+      description: newDescription,    // Modal subheading
+      contractFunction: newType,      // Name of function as it appears in the smart contract
+      formDefaults: newFormDefaults,  // JSON Object of default field values (used to pass dynamic initial vars)
+      callback: newCallback,          // Function to call on submit (allows custom construction of params etc)
+    }) => {
+      updateTransaction({ prevHash: null, prevSuccess: null });
+      dispatch({
+        open: true,
+        title: newTitle,
+        description: newDescription,
+        contractFunction: newType,
+        formDefaults: newFormDefaults,
+        callback: newCallback,
+      });
+    },
+    [updateTransaction]
+  );
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") return;
