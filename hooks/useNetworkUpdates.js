8bc7125370052e640cf29489b75f4f544e665ce3
FitFingers <hooper.j.1991@gmail.com>
2021-09-22 23:09:02 +0200

    feat: account changed notification

--- hooks/useNetworkUpdates.js
+++ hooks/useNetworkUpdates.js
@@ -14,10 +14,28 @@ export default function useNetworkUpdates(network) {
 
   // show feedback on successful network change
   useEffect(() => {
-    // if (!window.web3) return;
-    window.web3?.currentProvider?.on("chainChanged", (chain) => {
+    const handleNetworkChange = (chain) => {
       handleOpen("success", `Now using the ${chainID[chain] || chain} network`);
       window.location.reload();
-    });
+    };
+    window.ethereum?.on("chainChanged", handleNetworkChange);
+    return () =>
+      window.ethereum?.removeListener("chainChanged", handleNetworkChange);
+  }, [handleOpen]);
+
+  // show feedback on successful network change
+  useEffect(() => {
+    const handleAccountChange = ([account]) => {
+      handleOpen(
+        "success",
+        account
+          ? `Signed in to new account: ${account?.slice(-4)}`
+          : "Signed out"
+      );
+      // window.location.reload(); // TODO: is it necessary on account change?
+    };
+    window.ethereum?.on("accountsChanged", handleAccountChange);
+    return () =>
+      window.ethereum?.removeListener("accountsChanged", handleAccountChange);
   }, [handleOpen]);
 }
