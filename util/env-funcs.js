99c471a93db7d21c3751b7b088121b2708ff5658
FitFingers <hooper.j.1991@gmail.com>
2021-09-24 19:41:22 +0200

    feat: usefacotyraddress default to live addresses

--- util/env-funcs.js
+++ util/env-funcs.js
@@ -1,11 +1,8 @@
-const addresses = {
-  factory: {
-    dev: "0xF61377Fd98FCc77927c357C45735967f1300A40F", // must be manually updated (for ganache)
-    rinkeby: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS, // "0xA5BC059cb9F0D659A7e15C324e330f3adF12b095",
-  },
-};
+const dev = "0xF61377Fd98FCc77927c357C45735967f1300A40F"; // must be manually updated (for ganache)
+const rinkeby = process.env.NEXT_PUBLIC_RINKEBY_CONTRACT_ADDRESS; // "0xA5BC059cb9F0D659A7e15C324e330f3adF12b095"
+const mainnet = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 // TODO: figure out how to pass this via CLI!
-export function getFactoryAddress(env = "dev") {
-  return addresses.factory[env];
+export function getFactoryAddress() {
+  return mainnet || rinkeby || dev;
 }
